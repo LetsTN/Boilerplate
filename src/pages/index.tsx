76b7647f123ee86abda3d20127b1ac97519f9b1b
Lets TN <leticia.teix.nas@gmail.com>
2022-01-14 15:49:59 -0300

    chore: cria a pasta src

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
 
-export default function Home() {
+type Props = { title: string };
+
+export default function Home({ title = "AAAAAAAAAAAAAAAAAAA" }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,12 +14,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          aaaaaaaaaaaaaaaaaaaaaa
-        </h1>
+        <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,12 +58,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
