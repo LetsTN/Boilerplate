7279189b99f611c39f289f2706d1cee818ffa908
Lets TN <leticia.teix.nas@gmail.com>
2022-01-16 19:22:27 -0300

    chore: cofnigura os plugins de padronização e estilização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../../styles/Home.module.css";
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
 
-type Props = { title: string };
+type Props = { title: string }
 
-export default function Home({ title = "AAAAAAAAAAAAAAAAAAA" }: Props) {
+export default function Home({ title = 'AAAAAAAAAAAAAAAAAAA' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,7 +17,7 @@ export default function Home({ title = "AAAAAAAAAAAAAAAAAAA" }: Props) {
         <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
+          Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,12 +58,12 @@ export default function Home({ title = "AAAAAAAAAAAAAAAAAAA" }: Props) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  );
+  )
 }
